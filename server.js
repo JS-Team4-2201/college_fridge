634fa7c631e9daf93381251f4e115454c150c96c
Jeffrey Lee <dr3adnought@protonmail.com>
2022-03-21 17:12:31 -0400

    intial commit connection to MongoDB, package.json, server.js & package-lock.json w/ all node modules

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require('body-parser')
+const { sendFile } = require("express/lib/response");
+const res = require("express/lib/response");
+const { LEGAL_TCP_SOCKET_OPTIONS } = require("mongodb");
+const app = express();
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect("mongodb+srv://CFridge:BudgetFood2022@cluster0.ykuti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    useUnifiedTopology: true})
+    .then(client => {
+        console.log("Connected to Database")
+        const db = client.db("simple-recipes")
+        const recipesCollection = db.collection("recipes")
+
+
+        app.set("view engine", "ejs")
+        app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(express.static("public"))
+        app.use(bodyParser.json())
+
+        app.listen(3000, function() {
+            console.log("listening on 3000")
+        })
+    })
+    .catch(error => console.error(error))
