7bb6453fd32da9630c6de23c9a30c0b6a065012c
Daniel Harris <dan_harris10@yahoo.com>
2022-03-22 15:30:22 -0600

    minor fixes

--- public/script/script.js
+++ public/script/script.js
@@ -1,4 +1,4 @@
-import { getRecipes } from "../../public/third-party-API"
+import { getRecipes } from "../third-party-API/edamamAPI.js"
 
 // const variables for needed event handlers
 const add = document.querySelector(".add-btn")
