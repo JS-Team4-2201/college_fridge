cae2dc7057dd1edb00a7dcb78bc256e65160c0c5
Jimmy Vu <jimmy.h.vu_23@outlook.com>
2022-03-23 22:37:50 -0700

    Sending up script update for post method

--- routes/recipeRoute.js
+++ routes/recipeRoute.js
@@ -13,6 +13,15 @@ router.get("/", async (req, res, next) => {
     }
 })
 
+router.get("/recipes", async (req, res, next) => {
+    try{
+        const getRecipes = await recipes.find()
+        //res.render("index")
+        res.status(200).json({success: true, data: getRecipes})
+    } catch {
+        res.status(404).json({success: false, data: failMessage})
+    }
+})
 
 router.post("/", async (req, res) => {
     try{
